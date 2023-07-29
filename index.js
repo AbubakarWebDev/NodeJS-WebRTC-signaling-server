c440c3c2b997ec6e641cca358da5fdb49a2c38c1
Muhammad Abubakar <90tricks90@gmail.com>
2023-07-29 15:58:10 +0500

    Fixing Issues on file sharing

--- index.js
+++ index.js
@@ -96,126 +96,63 @@ const io = socketIO(server, {
     },
 });
 
-const room1Namespace = io.of("/room1");
-const room2Namespace = io.of("/room2");
-
-const r1users = {};
-const r1socketToRoom = {};
-
-room1Namespace.on('connection', (socket) => {
-    console.log(`A user with the socketId: ${socket.id} is connected`);
-
-    socket.on("join_room", (roomID) => {
-        if (r1users[roomID]) {
-            const length = r1users[roomID].length;
-
-            if (length === 2) {
-                socket.emit("room full");
-                return;
-            }
-
-            r1users[roomID].push(socket.id);
-        }
-        else {
-            r1users[roomID] = [socket.id];
-        }
-
-        r1socketToRoom[socket.id] = roomID;
-
-        const usersInThisRoom = r1users[roomID].filter((id) => id !== socket.id);
-
-        console.log("users: ", r1users);
-        console.log("socketToRoom: ", r1socketToRoom);
-        console.log("usersInThisRoom: ", usersInThisRoom);
-
-        socket.emit("all_peers", usersInThisRoom);
-    });
-
-    socket.on("sending_sdp_offer", (payload) => {
-        room1Namespace.to(payload.userToSignal).emit("receive_sdp_offer", {
-            signal: payload.signal,
-            callerID: payload.callerID,
-        });
-    });
-
-    socket.on("sending_sdp_answer", (payload) => {
-        room1Namespace.to(payload.callerID).emit("receive_sdp_answer", {
-            signal: payload.signal,
-            id: socket.id,
-        });
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`A user with the socketId: ${socket.id} is disconnected`);
-
-        const roomID = r1socketToRoom[socket.id];
-        const socketsInRoom = r1users[roomID];
-
-        if (socketsInRoom) {
-            const updatedSocketsInRoom = socketsInRoom.filter((id) => id !== socket.id);
-
-            r1users[roomID] = updatedSocketsInRoom;
-
-            delete r1socketToRoom[socket.id];
-
-            socket.broadcast.emit("user left", socket.id);
-        }
-    });
-});
-
-
 let initiator = null;
-const r2users = {};
-const r2socketToRoom = {};
 
-room2Namespace.on('connection', (socket) => {
+const users = {};
+const socketToRoom = {};
+
+io.on('connection', (socket) => {
     console.log(`A user with the socketId: ${socket.id} is connected`);
 
     socket.on("join_room", ({ roomID, init }) => {
-        r2users[roomID] ? r2users[roomID].push(socket.id) : r2users[roomID] = [socket.id];
+        users[roomID] ? users[roomID].push(socket.id) : users[roomID] = [socket.id];
 
-        r2socketToRoom[socket.id] = roomID;
+        socketToRoom[socket.id] = roomID;
 
-        if (init) {
-            initiator = socket.id;
-        }
-        else {
-            room2Namespace.to(initiator).emit("peer_connected", socket.id);
-        }
-
-        console.log("users: ", r2users);
-        console.log("initiator: ", initiator);
-        console.log("socketToRoom: ", r2socketToRoom);
+        init ? initiator = socket.id : io.to(initiator).emit("peer_connected", socket.id);
     });
 
     socket.on("sending_sdp_offer", (payload) => {
-        room2Namespace.to(payload.userToSignal).emit("receive_sdp_offer", {
+        io.to(payload.userToSignal).emit("receive_sdp_offer", {
             signal: payload.signal,
             callerID: payload.callerID,
         });
     });
 
     socket.on("sending_sdp_answer", (payload) => {
-        room2Namespace.to(payload.callerID).emit("receive_sdp_answer", {
+        io.to(payload.userToSignal).emit("receive_sdp_answer", {
             signal: payload.signal,
-            id: socket.id,
+            callerID: payload.callerID,
+        });
+    });
+
+    socket.on("sending_file_MetaData", (payload) => {
+        io.to(payload.userToSignal).emit("receive_file_MetaData", {
+            metaData: payload.metaData,
+            callerID: payload.callerID,
+        });
+    });
+
+    socket.on("successfully_store_file_MetaData", (payload) => {
+        io.to(payload.userToSignal).emit("peer_successfully_store_file_MetaData", {
+            callerID: payload.callerID,
         });
     });
 
     socket.on('disconnect', () => {
         console.log(`A user with the socketId: ${socket.id} is disconnected`);
 
-        const roomID = r2socketToRoom[socket.id];
-        const socketsInRoom = r2users[roomID];
+        const roomID = socketToRoom[socket.id];
+        const socketsInRoom = users[roomID];
 
         if (socketsInRoom) {
             const updatedSocketsInRoom = socketsInRoom.filter((id) => id !== socket.id);
 
-            r2users[roomID] = updatedSocketsInRoom;
+            users[roomID] = updatedSocketsInRoom;
 
-            delete r2socketToRoom[socket.id];
+            delete socketToRoom[socket.id];
 
-            socket.broadcast.emit("user left", socket.id);
+            socket.broadcast.emit("user_left", socket.id);
         }
     });
 });
